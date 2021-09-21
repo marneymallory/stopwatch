cb3be92cf47a784c869f7c36b93dd6baaec065e7
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-09-21 11:32:21 -0800

    first commit, ad stop, resume, start functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [time, setTime] = React.useState(0)
+  const [timerOn, setTimeOn] = React.useState(false)
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if(timerOn) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10)
+    } else {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+    
+  }, [timerOn])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div>
+        {!timerOn && time === 0 &&(
+        <button onClick={() => setTimeOn(true)}>Start</button>
+        )}
+        {timerOn && (
+        <button onClick={() => setTimeOn(false)}>Stop</button>
+        )}
+        {!timerOn && time !== 0 &&(
+        <button onClick={() => setTimeOn(true)}>Resume</button>
+        )}
+        {!timerOn && time > 0 &&(
+        <button onClick={() => setTime(0)}>Reset</button>
+        )}
+      </div>
     </div>
   );
 }
