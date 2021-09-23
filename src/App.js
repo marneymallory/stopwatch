04fd15fbc257dfee3b64ad44897e14d45f3984a1
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-09-23 10:46:13 -0800

    fixed styling component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from "react";
 import "@ionic/react/css/core.css";
 import { IonButton } from "@ionic/react";
+import "./core.css"; 
 
-// /* Core CSS required for Ionic components to work properly */
-// import '@ionic/react/css/core.css'
-
-// /* Basic CSS for apps built with Ionic */
-// import '@ionic/react/css/normalize.css'
-// import '@ionic/react/css/structure.css'
-// import '@ionic/react/css/typography.css'
-
-// /* Optional CSS utils that can be commented out */
-// import '@ionic/react/css/padding.css'
-// import '@ionic/react/css/float-elements.css'
-// import '@ionic/react/css/text-alignment.css'
-// import '@ionic/react/css/text-transformation.css'
-// import '@ionic/react/css/flex-utils.css'
-// import '@ionic/react/css/display.css'
-
-// /* Theme variables */
-// import 'theme/variables.css'
-// import 'theme/styles.css'
 
 function App() {
   const [time, setTime] = React.useState(0)
